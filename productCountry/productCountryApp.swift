0b441fe204d472179d63732ffc15451f8e57dcfa
hengdda <hengdda@hotmail.com>
2025-02-06 16:37:27 -0500

    Initial Commit

---
+++ productCountry/productCountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  productCountryApp.swift
+//  productCountry
+//
+//  Created by Mac22N on 2025-02-06.
+//
+
+import SwiftUI
+
+@main
+struct productCountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
